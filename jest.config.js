415b4c578843e3598f7de74aeeb05db7cc6f8639
ruzor <jwokomaty@gmail.com>
2020-07-22 18:09:03 +0100

    chore: add test

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   testRegex: ['.spec.js$', '.test.js$'],
   coverageDirectory: './coverage',
   testEnvironment: 'node',
+  // testTimeout: 30000,
   // setupFilesAfterEnv: ['./__tests__/config/setup.js']
   coveragePathIgnorePatterns: ['/node_modules/']
 };
