b9e24f807dab20f42d24decb60850fb39f92608e
ruzor <jwokomaty@gmail.com>
2020-07-23 19:25:30 +0100

    chore: add README file for ./src

---
+++ src/config/mongodb.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const { connectionUri } = require('./utils/dbParams');
+
+const connect = async () => {
+  if (mongoose.connection.readyState === 0) {
+    try {
+      await mongoose.connect(connectionUri, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+      });
+    } catch (err) {
+      console.error('App starting error:', err.stack);
+      process.exit(1);
+    }
+  }
+};
+
+const truncate = async () => {
+  if (mongoose.connection.readyState !== 0) {
+    const { collections } = mongoose.connection;
+
+    const promises = Object.keys(collections).map((collection) =>
+      mongoose.connection.collection(collection).deleteMany({})
+    );
+
+    await Promise.all(promises);
+  }
+};
+
+const disconnect = async () => {
+  if (mongoose.connection.readyState !== 0) {
+    await mongoose.disconnect();
+  }
+};
+
+module.exports = {
+  connect,
+  truncate,
+  disconnect
+};
