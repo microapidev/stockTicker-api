5a9da7493322f22f99b819d2a7adb09870baf261
ruzor <jwokomaty@gmail.com>
2020-07-23 14:30:44 +0100

    chore: add files

--- __test__/server.test.js
+++ __test__/server.test.js
@@ -2,9 +2,8 @@ import app from '../src/app';
 const request = require('supertest');
 
 describe('Dummy Test', () => {
-  jest.setTimeout(30000);
   test('should respond with status code 200', async () => {
     const res = await request(app).get('/');
     expect(res.status).toBe(200);
-  });
+  }, 10000);
 });
---
+++ src/utils/customResponse.js
@@ -0,0 +1,8 @@
+module.exports = (res, status, data, message) => {
+    res.status(status).json({
+      data,
+      status: "success",
+      message,
+    });
+  };
+  
