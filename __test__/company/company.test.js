a93fec68d5c375e02a8e0a3b3f27b32ec30d028a
ruzor <jwokomaty@gmail.com>
2020-07-25 01:56:37 +0100

    fix: add and debug tests

---
+++ __test__/company/company.test.js
@@ -0,0 +1,23 @@
+import app from '../../src/app';
+import request from 'supertest';
+
+describe('Financial Info', () => {
+  test('should respond with status code 200', async () => {
+    const res = await request(app).get('/api/v1/company/GOOGL/metric');
+    expect(res.status).toBe(200);
+  }, 10000);
+});
+
+describe('Company Info', () => {
+  test('should respond with status code 200', async () => {
+    const res = await request(app).get('/api/v1/company/GOOGL/profile');
+    expect(res.status).toBe(200);
+  }, 10000);
+});
+
+describe('Company Stock Quote', () => {
+  test('should respond with status code 200', async () => {
+    const res = await request(app).get('/api/v1/company/GOOGL/stock');
+    expect(res.status).toBe(200);
+  }, 10000);
+});
--- __test__/server.test.js
+++ __test__/server.test.js
@@ -1,23 +1,9 @@
 import app from '../src/app';
-const request = require('supertest');
+import request from 'supertest';
 
 describe('Dummy Test', () => {
   test('should respond with status code 200', async () => {
     const res = await request(app).get('/');
-    expect(res.status).toBe(200);
-  }, 10000);
-});
-
-describe('Financial Info', () => {
-  test('should respond with status code 200', async () => {
-    const res = await request(app).get('/v1/metric?symbol=GOOGL');
-    expect(res.status).toBe(200);
-  }, 10000);
-});
-
-describe('Company Info', () => {
-  test('should respond with status code 200', async () => {
-    const res = await request(app).get('/v1/companyProfile?symbol=GOOGL');
     expect(res.status).toBe(200);
   }, 10000);
 });
