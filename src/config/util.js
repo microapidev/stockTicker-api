ed095a1eff338d126a9261da2eed1adb8b67bb0f
ruzor <jwokomaty@gmail.com>
2020-07-25 12:10:43 +0100

    fix: tests

--- src/config/util.js
+++ src/config/util.js
@@ -4,9 +4,10 @@ let PORT = process.env.PORT;
 let DB_URI = process.env.DB_URI;
 let HOST = process.env.HOST;
 let ADMIN_SECRET = process.env.ADMIN_SECRET;
+let FINNHUB_APIKEY = process.env.FINNHUB_APIKEY;
 
 if (process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'dev') {
   DB_URI = process.env.DB_URI;
 }
 
-module.exports = { ADMIN_SECRET, DB_URI, HOST, PORT };
+module.exports = { ADMIN_SECRET, DB_URI, FINNHUB_APIKEY, HOST, PORT };
